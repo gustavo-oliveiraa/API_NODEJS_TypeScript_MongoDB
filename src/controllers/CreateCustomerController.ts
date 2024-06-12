0ee609e8ee419b594a530729c97068cbe9f91995
Gustavo <maxwelgustavo@gmail.com>
2024-06-12 02:37:09 -0300

    Parei à aula no tempo 28:42

---
+++ src/controllers/CreateCustomerController.ts
@@ -0,0 +1,8 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { CreateCustomerService } from '../services/CreateCustomerService'
+
+class CreateCustomerController {
+  async handle(request: FastifyRequest, reply: FastifyRequest) {
+ 
+  }
+}
